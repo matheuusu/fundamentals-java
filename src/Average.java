1cf34f4bcc4cbfb0fb08a4c94440a172c6cbd9ff
matheus <devtheus0@gmail.com>
2024-09-13 12:17:41 -0300

    feat(Average): Added media class for notes

---
+++ src/Average.java
@@ -0,0 +1,9 @@
+public class Average {
+    public static void main (String[] args) {
+        double firstNote = 7.5;
+        double secondNote = 8.7;
+        double media = (firstNote + secondNote) / 2;
+
+        System.out.printf("Sua media é %.1f", media);
+    }
+}
