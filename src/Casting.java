d3fcce908ec1df1aa7327de5f47b7619d134f340
matheus <devtheus0@gmail.com>
2024-09-13 12:48:23 -0300

    feat(Casting): Added casting class for notes

---
+++ src/Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main (String[] args) {
+        double firstNote = 7.3;
+        int secondNote = 5;
+        int sum = (int) (firstNote + secondNote);
+
+        System.out.println(sum);
+    }
+}
