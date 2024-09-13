42497eaf60ccce773f21188471c5aee5f0f092f2
matheus <devtheus0@gmail.com>
2024-09-13 13:06:47 -0300

    feat(TextInterpolator): Added a text interpolator class

---
+++ src/TextInterpolator.java
@@ -0,0 +1,8 @@
+public class TextInterpolator {
+    public static void main (String[] args) {
+        char letter = 'e';
+        String name = "Matheus";
+
+        System.out.printf("Olá, %s %s Silva!", name, letter);
+    }
+}
